9199af94f034f3cc3fa69793b9125a231e38c978
Fadıl Başaran <fadil.basaran.014@gmail.com>
2022-03-01 16:16:41 +0300

    color chance

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.yellow,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
