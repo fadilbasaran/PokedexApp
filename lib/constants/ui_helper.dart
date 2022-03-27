8be4aaac4b0d8e02cc71f8e6f79d2bd635afae46
Fadıl Başaran <fadil.basaran.014@gmail.com>
2022-03-27 15:47:45 +0300

    Kaldığım yere geldim ve açıklama satırları bitti.Uygulamayı yapmaya devam ediliyor.

--- lib/constants/ui_helper.dart
+++ lib/constants/ui_helper.dart
@@ -2,8 +2,18 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class UIHelper {
-  UIHelper._();
+  UIHelper._(); //Gizli ve kimse bir nesne oluşturmasın
   static double getAppTitleWidgetHeigth() {
     return ScreenUtil().orientation == Orientation.portrait ? 0.15.sh : 0.15.sw;
+    /*Eğer portait(dikey) modda ise ekran yüksekliğinin %15 yap, 
+    değils yatay modda ise ekran genişliğinin %15'ni yap*/
+  }
+
+  static EdgeInsets getDeafultPadding() {
+    if (ScreenUtil().orientation == Orientation.portrait) {
+      return EdgeInsets.all(12.h);//Eğer dikey modda ise ekran yükskeliği üzerinden %12 olarak ayarla.
+    } else {
+      return EdgeInsets.all(8.w);//Eğer yatay modda ise ekran genişliği üzerinden %8 olarak ayarla.
+    }
   }
 }
--- lib/widgets/app_title.dart
+++ lib/widgets/app_title.dart
@@ -15,24 +15,27 @@ class _AppTitleState extends State<AppTitle> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return SizedBox(
       height: UIHelper.getAppTitleWidgetHeigth(),
-      color: Colors.red,
       child: Stack(
-        
         children: [
-          Align(
-              alignment: Alignment.topLeft,
-              child: Text(
-                Constants.title,
-                style: Constants.getTitleTextStyle(),
-              )),
+          Padding(
+            padding: EdgeInsets.all(8.w),
+            child: Align(
+                alignment: Alignment.centerLeft, //Yazı ortalması için
+                child: Text(
+                  Constants.title,
+                  style: Constants.getTitleTextStyle(),
+                )),
+          ),
           Align(
             alignment: Alignment.topRight,
             child: Image.asset(
               _pokeballImageUrl,
-              width: 100.w,
-              height: 100.h,//Genişlik ve yükselik oranları
+              width: ScreenUtil().orientation == Orientation.portrait
+                  ? 0.20.sh
+                  : 0.20.sw,
+              //Dikey modda ise yüksekliğin %20'si,yatay modda ise genişliğin %20'si olacak şeklide ayarla.
               fit: BoxFit.fitWidth,
             ),
           ),
